adf24a71aac2c502f944ad8e2cf75e2e83f0b3f5
Kandice <kandice.levero@gmail.com>
2016-05-01 22:47:53 -0400

    Second Commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+/********** NAV ITEM HOVER ************/
+
+/*$('.nav_item').hover(
+	function() {
+    	$(this).parent().find('.nav_bkg').stop().animate({opacity: 1}, 800);
+    }, 
+    function() {
+        $(this).parent().find('.nav_bkg').stop().animate({opacity: 0}, 0);
+    }
+);*/
+
